0baab4456aaa0c2c4aec04591cea457a72862a2e
kdharsh24 <harshkumardas24@gmail.com>
2025-03-19 15:02:21 +0530

    backend floor

--- backend/try.js
+++ backend/try.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { MongoClient, ObjectId } = require('mongodb');
 const cors = require('cors');
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { emit } = require('nodemon');
 
 const app = express();
@@ -489,6 +489,6 @@ io.on("connection", (socket) => {
 // Start the server
 connectToDatabase().then(() => {
   httpServer.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://Vercel`);
   });
 });
